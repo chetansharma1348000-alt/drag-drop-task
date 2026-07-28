499d572d79682853b0c1b882c3d2d428776d6873
Chetan Sharma <chetansharma1348000@gmail.com>
2026-07-28 15:34:44 +0530

    Create Task.jsx

---
+++ src/components/Task.jsx
@@ -0,0 +1,56 @@
+function Task({ task, columnKey }) {
+  const autoScroll = (e) => {
+    const threshold = 120;
+    const speed = 15;
+
+    const y = e.clientY;
+
+    if (y < threshold) {
+      window.scrollBy({
+        top: -speed,
+        behavior: "auto",
+      });
+    }
+
+    if (y > window.innerHeight - threshold) {
+      window.scrollBy({
+        top: speed,
+        behavior: "auto",
+      });
+    }
+  };
+
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData("task", task);
+    e.dataTransfer.setData("source", columnKey);
+
+    document.addEventListener("dragover", autoScroll);
+
+    setTimeout(() => {
+      e.target.classList.add("dragging");
+    }, 0);
+  };
+
+  const handleDragEnd = (e) => {
+    e.target.classList.remove("dragging");
+    document.removeEventListener("dragover", autoScroll);
+  };
+
+  return (
+    <div
+      className="task"
+      draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      <div className="task-left">
+        <span className="drag-icon">☰</span>
+        <span className="task-title">{task}</span>
+      </div>
+
+      <span className="task-arrow">→</span>
+    </div>
+  );
+}
+
+export default Task;
